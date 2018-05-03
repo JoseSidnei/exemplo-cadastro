1261b9329877332cc7e8e9a5a1c066d7d32615f7
JoseSidnei <38699945+JoseSidnei@users.noreply.github.com>
2018-05-03 17:29:11 -0300

    adicionado exemplo de cadastro

---
+++ Sistema.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ExemploSistemaComForWhileVetor
+{
+    class Sistema
+    {
+        Barco barco = new Barco();
+        public Sistema()
+        {
+            GeriMenu();
+        }
+        // apresentar o menu para o usuário
+        public void Menu()
+        {
+            Console.WriteLine(
+                @"
+1 - Cadastro de barcos
+2 - Ediar Barco
+3 - Listar Barcos
+4 - Buscar Barco
+20 - Sair");
+        }
+        public void GeriMenu()
+        {
+            /*
+             * Chama método para apresentar o menu
+             * para o usuário 
+             */
+            Menu();
+            int menuEscolha = Convert.ToInt32(Console.ReadLine());
+            while (menuEscolha != 20)
+            {
+                switch (menuEscolha)
+                {
+                    case 1:
+                        barco.CadastroBarcos();
+                        break;
+                    case 2:
+                        barco.EditarBarco();
+                        break;
+                    case 3:
+                        barco.ListarBarcos();
+                        break;
+                    case 4:
+                        barco.BuscarBarco();
+                        break;
+                }
+            Menu();
+            menuEscolha = Convert.ToInt32(Console.ReadLine());
+            }
+           
+           
+
+
+        }
+    }
+}
---
+++ barco.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExemploSistemaComForWhileVetor
+{
+    class Barco
+    {
+        string[] barcosNomes = new string[100];
+        string[] barcosCor = new string[100];
+        double[] barcosPreco = new double[100];
+        int atual = 0;
+
+        public void CadastroBarcos()
+        {
+            Console.Clear();
+            Console.Write("Nome do barco: ");
+            barcosNomes[atual] = Console.ReadLine();
+            Console.Write("Cor do barco: ");
+            barcosCor[atual] = Console.ReadLine();
+            Console.Write("Preço do barco: ");
+            barcosPreco[atual] = Convert.ToDouble(Console.ReadLine());
+            atual++;
+        }
+
+        public void ListarBarcos()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista de barcos ");
+            for (int i = 0; i < atual; i++)
+            {
+                Console.WriteLine(
+                    String.Format(
+                        "\nNome: {0} \nCor: {1} \nPreço: {2}",
+                        barcosNomes[i], barcosCor[i], barcosPreco[i]
+                        )
+                    );
+            }                              
+        }
+
+        public void EditarBarco()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite um nome para pesquisa");
+            string nomePesquisado = Console.ReadLine().ToLower().Trim();
+            int posicao = -1;
+            for (int i = 0; i < atual; i++)
+            {
+                if (nomePesquisado == barcosNomes[i])
+                {
+                    posicao = i;
+                }
+            }
+
+            if (posicao != -1)
+            {
+                // encontrou o registro procurado
+                Console.Write("Nome do barco: ");
+                barcosNomes[atual] = Console.ReadLine();
+                Console.Write("Cor do barco: ");
+                barcosCor[atual] = Console.ReadLine();
+                Console.Write("Preço do barco: ");
+                barcosPreco[atual] = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Dados alterados com sucesso");
+            }
+            else
+            {
+                Console.WriteLine("Registro não encontrado");
+                
+            }
+        }
+
+        internal void BuscarBarco()
+        {
+            Console.WriteLine("Digite o nome para busca: ");
+            string nomeBuscado = Console.ReadLine();
+            bool achou = false;
+            for (int i = 0; i < atual; i++)
+            {
+                if (nomeBuscado == barcosNomes[i])
+                {
+                    Console.WriteLine(
+                        String.Format(
+                            "\nNome: {0} \nCor: {1} \nPreço: {2}",
+                            barcosNomes[i], barcosCor[i], barcosPreco[i]
+
+                        )
+                    );
+                    achou = true;
+                    break;
+                }
+            }
+            if (!achou)
+            {
+                Console.WriteLine("Nome não encontrado");
+            }
+        }
+    }       
+}           
+            
